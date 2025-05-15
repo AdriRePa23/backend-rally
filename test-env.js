5003bd8553a161e8f63f446ffec506fbac0b7d7d
adrian <adrianreal234@gmail.com>
2025-05-15 22:49:34 +0200

    actualizar la versión de dotenv a 16.5.0 y eliminar archivos de prueba de correo electrónico y entorno

--- test-env.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config({ path: "c:/Users/adria/Documents/Instituto/ProyectoIntegrado/backend-rally/.env" });
-
-console.log("SENDGRID_API_KEY:", process.env.SENDGRID_API_KEY);
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
